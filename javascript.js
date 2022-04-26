cd53272a395a7b511986521bb398d73dbc11a4a7
Devon <dev.kahika.campbell@gmail.com>
2022-04-26 17:16:45 +1200

    Start creating function to play round.
    Create function that takes two parameters player and cpu selection and
    assigns a new variable choice with an integer associated with the
    selection, Rock is 0, paper 1, scissors 2. This will make it easier when
    writing conditionals.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,37 @@
+// Function that get's PC to choose between 3 numbers to represent rock, paper, scissors.
 function computerPlay() {
-    let choice = ["rock", "paper", "scissors"]
+    let choice = ['rock' ,'paper', 'scissors']
     return choice[Math.floor(Math.random() * choice.length)]
 }
+// Function with two parameters player choice and CPU choice will return win or loss
 
-function playRound(player_play) {
-    if ((toString(player_play)) === "rock");
-        if ((computerPlay()) === "scissors") {
-            console.log("you win bitch")
-        }
-    else {
-        console.log("something else happened")
+function playRound(playerSelection, computerSelection) {  
+    // Take selections and assign them new number variables 1, 2, 3 to represent rock, paper, scissors.
+    playerSelectionLower = playerSelection.toLowerCase();
+    if (playerSelectionLower.includes('rock')) {
+        playerChoice = 0;
+    }
+    else if (playerSelectionLower.includes('paper')) {
+        playerChoice = 1;
+    }
+    else if (playerSelectionLower.includes('scissors')) {
+        playerChoice = 2;
+    }
+
+    if (computerSelection.includes('rock')) {
+        cpuChoice = 0;
+    }
+    else if (computerSelection.includes('paper')) {
+        cpuChoice = 1;
+    }
+    else if (computerSelection.includes('scissors')) {
+        cpuChoice = 2;
+    }
+    // Test to see if selection converts to integer.
+    console.log(`Player = ${playerChoice}/${playerSelection}, CPU = ${cpuChoice}/${computerSelection}`)
+    
+    // If selection number is the same declare draw.
+    if (playerChoice === cpuChoice) {
+        return `You chose ${playerSelection}, CPU chose ${computerSelection}, It's a Draw!`;
     }
 }
